7d8c408a9cda4d7d6d90dbd703905a971c83d248
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-01-22 07:38:37 +0000

    Jan 22, 2025, 7:38 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,2 +1,2 @@
 # print("Hello from lesson 3")
-# so basically today is alot of coding and coding
+# so basically today is alot of coding and coding and coding and codingand codingand codingand coding
