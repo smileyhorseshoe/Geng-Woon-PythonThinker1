26189b4bef15df138ce50a23be0ded656a9f042c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-01-22 07:44:54 +0000

    Jan 22, 2025, 7:44 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -3,7 +3,11 @@
 
 
 
-# Recap 1:
-# 
-#
-#
+## Recap 1: Pseudo Code Practice
+# Write a pseudocode that calculates the average score of the 4
+# students:
+
+# Adam: 60
+# Brandon: 58
+# Charlie: 98
+# Daniel: 79
