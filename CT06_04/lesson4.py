b3ba8c504ee69a9f9ec772897f7dfe938dd8fd15
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-05 08:01:18 +0000

    Feb 5, 2025, 8:01 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -20,15 +20,16 @@
 #  Calculating red plates
 #  1 red plate = $3
 #  1 x $3 = $3
-redPlates = 3
-#  Calculating blue plates
-#  5 x $2 = $10
-bluePlates = 10
-# Calculating Green plates
-#  4 x $3 = $12
-greenPlates = 12
+# redPlates = 3
+# #  Calculating blue plates
+# #  5 x $2 = $10
+# bluePlates = 10
+# # Calculating Green plates
+# #  4 x $3 = $12
+# greenPlates = 12
 # Calculating total
 # ans = greenPlates + bluePlates + redPlates
 
 # Printing amount
 # print("Your total is", ans, "dollars.")
+#############3
