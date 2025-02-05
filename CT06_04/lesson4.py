7ebb47c8503da866b7d91a4aae02a610d18710a2
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-05 08:03:55 +0000

    Feb 5, 2025, 8:03 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -32,5 +32,19 @@
 
 # Printing amount
 # print("Your total is", ans, "dollars.")
-myName = input("What is your name?")
-print("Hello", myName, "welcome to coding!")
+# myName = input("What is your name?")
+# print("Hello", myName, "welcome to coding!")
+
+## Task 1: Storing and printing Strings
+
+# **Task 1a**:
+# Use the input() function to ask the user for their name and
+# store it in a variable. Then print that variable.
+
+# **Task 1b**:
+# Ask the user for their favorite colour using input() and
+# store the response in a variable. Print the variable.
+
+# **Task 1c**:
+# Ask the user for their age using input() and store the answer
+# in a variable. Print the variable.
