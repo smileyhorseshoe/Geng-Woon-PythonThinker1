50402cbc74802505db6d8254be8736b4229458fe
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-05 08:25:53 +0000

    Feb 5, 2025, 8:25 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -96,4 +96,4 @@ print("Your favourite hobby is" + myHobby)
 #  TASK 2C
 dreamVacation = input("What is your dream vacation destination?")
 print("I would like to vist" + dreamVacation)
-print("Hello," + myName + ",Your hobby is" + myHobby + "Your dream vacation holiday place is" + dreamVacation)
+print("Hello," + myName + "Your hobby is" + myHobby + "Your dream vacation holiday place is" + dreamVacation)
