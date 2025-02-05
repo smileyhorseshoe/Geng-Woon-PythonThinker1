7fa6a8b58097aae41c4dc4dd11aecda8a7e0be06
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-05 08:48:53 +0000

    Feb 5, 2025, 8:48 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -122,5 +122,5 @@
 # 3. Convert the age back to a string and print "You are [age]
 # years old".]
 age = int(input("What is your age?"))
-age + 1
-print("Next year, you will be" + str(age))
+age + 1 = age + 1
+print("Next year, you will be " + str(age))
