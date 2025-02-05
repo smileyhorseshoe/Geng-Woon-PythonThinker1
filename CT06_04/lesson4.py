e9b16a2aa1e5f3e2c2e631c1cb1b9f206d9622ad
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-05 07:49:44 +0000

    Feb 5, 2025, 7:49 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,3 +1,20 @@
 print("Hello from lesson 4")
 part = 5
 print(part)
+
+# Lesson 4 - Strings
+
+## Recap 1: Sushi Checkout
+
+# You just had a lunch at a sushi restaurant and have to
+# calculate the total amount you have spent. Different coloured
+# plates costs different as shown below:
+
+# Red = $1
+# Blue = $2
+# Green = $3
+
+# You have ordered a total of 3 red plates, 5 blue plates, and 4
+# green plates.
+
+# Calculate and print the total amount you have spent: -->
