03b5d982d4c30c875b73ad97961e9fe02deff475
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-12 07:44:08 +0000

    Feb 12, 2025, 7:44 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,18 @@
-# print("Hello from lesson 5")
+# Lesson 5 - Introduction to For Loop and range()
+
+## Recap 1: Automated Birthday Invitation
+
+# You run a small business that creates personalized digital
+# birthday invitation cards. To automate the process, you decide
+# to write a Python program. 
+
+# This program should ask the user for:
+# 1. birthday person's name
+# 2. the age that they are turning that year
+# 3. personal message from the sender
+
+# Finally, the program prints out a personalized invitation
+# message.
+
+# ### Sample output:
+# "Happy <Age>th birthday <Name>! <Message>"
