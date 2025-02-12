758ee8f013cabce7f4ab97b0cc05d1d9f74356ca
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-12 08:04:22 +0000

    Feb 12, 2025, 8:04 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -20,3 +20,5 @@ birthdayName = input("What is your name?")
 age = int(input("What is your age?"))
 msg = input("What is a special message you'd like to send?")
 print("Happy " + str(age)+"th birthday," + birthdayName  + "!" + msg)
+for i in range(100):
+    print("I will not sling mud at my friends again.")
