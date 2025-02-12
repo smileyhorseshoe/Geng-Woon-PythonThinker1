e9b98bb60cf92fd08202a92fe7e67d2deadcb49f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-12 08:28:21 +0000

    Feb 12, 2025, 8:28 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -47,8 +47,9 @@
 # Note:
 #     Notice how "Give me a..." is repeated!
 #     Which function should you be using?
-Name = input("WHat is your name?")
-for char in Name:
-    print("Give me a "+ char + "!")
-print("What do we have?")
-print(Name + "!")
+# TASK 1
+# Name = input("WHat is your name?")
+# for char in Name:
+#     print("Give me a "+ char + "!")
+# print("What do we have?")
+# print(Name + "!")
