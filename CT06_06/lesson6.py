c14aa372cf28248fa51122fda54aaa08524409ee
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-19 07:56:11 +0000

    Feb 19, 2025, 7:56 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -12,5 +12,5 @@
 # Use only variables, math operators that you have learnt, as
 # well as a 'for' loop.
 StudentNum = input("How many people do you have in your class?")
-Marks = int(input("What are their marks?"))
+Marks = input("What are their marks?")
 print(Marks)
