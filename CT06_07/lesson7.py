8b7a3145f3e68f697417a12b7c4570f727302988
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 07:43:06 +0000

    Feb 26, 2025, 7:43 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -5,14 +5,14 @@
 ### There is a total of 3 bugs in the following program.
 ### Identify and fix the bugs:
 
-# score_one = 80
-# score_two = 90
-# score_three = 75
+score_one = 80
+score_two = 90
+score_three = 75
 
-# total = score_one + score_two score_three
+total = score_one + score_two + score_three
+#  Changes made: "score_two + score+Three"
+average_score = total / 3
 
-# average_score = totl / 3
+student_name = "Alex"
 
-# student_name = "Alex"
-
-# print("Average score for " + student_name + " is: " + average_score)
+print("Average score for " + student_name + " is: " + average_score)
