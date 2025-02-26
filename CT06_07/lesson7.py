043dcdb1242a9777a317e074ffafc9901f9fc4b5
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 08:37:20 +0000

    Feb 26, 2025, 8:37 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -146,6 +146,6 @@
 # Task 6
 sum = 0
 for i in range(1 , 6):
-    sum = sum + = int(input("What is number # "+str(i) + "?"))
-    
+    sum = sum + int(input("What is number # "+str(i) + "?"))
+
 print("The sum of the 5 numbers you have chosen is " + str(sum))
