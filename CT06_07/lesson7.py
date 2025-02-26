20a632ac7e083f74599893e149df033b0b7d65cb
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 07:42:24 +0000

    Feb 26, 2025, 7:42 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,1 +1,18 @@
-print("Hello from lesson 7")
+# Lesson 7 - For Loop (Part 2)
+
+## Recap 1: Debugging Average Score Program
+
+### There is a total of 3 bugs in the following program.
+### Identify and fix the bugs:
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two score_three
+
+# average_score = totl / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + average_score)
