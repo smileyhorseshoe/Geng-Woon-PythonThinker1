1f462256a6cb7be61a8db5ff30666bc927d11c2f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 08:47:27 +0000

    Feb 26, 2025, 8:47 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -165,7 +165,7 @@
 # 5 x 12 = 60
 
 numberTimes = int(input("What number for the timestable in numeral(will be printed 12 times.)"))
-for i in range(12):
-    print( str(numberTimes) + " x" , str(i) , "+" , str(numberTimes * i))
+for i in range(1):
+    print( str(numberTimes) + " x" , str(i) , "=" , str(numberTimes * i))
 
     
