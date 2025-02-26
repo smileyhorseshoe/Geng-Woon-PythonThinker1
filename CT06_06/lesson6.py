e8189e8c1a4d6ce458ce41459e0652dcc74ffb0b
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 07:06:02 +0000

    Feb 26, 2025, 7:06 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -11,10 +11,93 @@
 
 # Use only variables, math operators that you have learnt, as
 # well as a 'for' loop.
-StudentNum = int(input("How many people do you have in your class?"))
-Marks = 0
-for StudentNum in range(StudentNum):
-    Marks = Marks + int(input("What are their marks?"))
-Marks = Marks / StudentNum
-print("My sum is:" + str(Marks))
-print("My average is " + str(Marks / StudentNum))
+# StudentNum = int(input("How many people do you have in your class?"))
+# Marks = 0
+# for StudentNum in range(StudentNum):
+#     Marks = Marks + int(input("What are their marks?"))
+# print("My sum is:" + str(Marks))
+# print("My average is " + str(Marks / StudentNum))
+
+## Task 1: Syntax Errors
+# Fix the errors in the following:
+
+# **Task 1a**:
+# for i in range(3):
+    # print("Hello, World!")
+## Changes made:
+# Added a colon after the '(3)'.
+# **Task 1b**:
+# for i in range(5):
+    # print(i)
+#  Changes made:
+# Indented line 31.
+# **Task 1c**:
+# print("Hello, World!")
+#  Changes made:
+# Added the closing inverted coma at the end of '!'
+# **Task 1d**:
+# If = 5
+# Changes made:
+# Captalized the "If"
+# **Task 1e**:
+# print ("Hello, World!")
+# Changes made:
+#  Added the brackets
+
+## Task 2: Name Errors
+# Fix the errors in the following:
+
+# # **Task 2a**:
+# age = int(input("What is your age?"))
+# print(age)
+# # Changes made:
+# # Added input to ask the user for their age.
+# # **Task 2b**:
+# name = "Alice"
+# print(name)
+# # Changes made:
+# # Added a 'e' to line 7
+# # **Task 2c**:
+# x = 5
+# print(x)
+# # Changes made:
+# # Reversed the order
+
+# # **Task 2d**:
+# print("Hello, World!")
+# # Changes made:
+# # added a 'r' to print
+
+## Task 3: Type Errors
+# Fix the errors in the following:
+
+# **Task 3a**:
+age = 25
+print(age + 1)
+# Changes made:
+#  removed the inverted commas
+# **Task 3b**:
+number = 10
+print(number - 5)
+# Changes made:
+# Removed the inverted comma line 81
+# **Task 3c**:
+print("Repeat" * 3)
+# changes made:
+# Removed inverted commas
+# **Task 3d**:
+year = 2023
+print("The year is " + str(year))
+# Changes made
+# Changed the variable 'year' to a str 
+# **Task 3e**:
+x = 10
+y = x / 2
+# Changes made:
+# Removed inverted commas during assignment of varible 'x'
+# **Task 3f**:
+end = 5
+for i in range(end):
+    print(i)
+# Changes made:
+# Removed inverted commas during assignment of varible 'end'
