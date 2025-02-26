8f2b15ffd2684b77719fc920eee577ef44422e40
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-02-26 07:48:41 +0000

    Feb 26, 2025, 7:48 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -5,17 +5,17 @@
 ### There is a total of 3 bugs in the following program.
 ### Identify and fix the bugs:
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
-#  Changes made: "score_two + score+Three".
-average_score = total / 3
+# total = score_one + score_two + score_three
+# #  Changes made: "score_two + score+Three".
+# average_score = total / 3
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + str(average_score))
+# print("Average score for " + student_name + " is: " + str(average_score))
 # Changes made: Changed variable average_score to string from integer.
 
 ## Task 9: Accumulative Sum in Loop
