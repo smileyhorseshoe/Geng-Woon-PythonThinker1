91762723c05f1bec8ed31dbc6e6b38fcbc75510a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-05 08:00:23 +0000

    Mar 5, 2025, 8:00 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,7 +4,8 @@
 # # Printing a welcome script with their name given in the input.
 # 
 start = input("What is your starting number?")
-# Asking the user for the first number via a input(variable named "start")
+# Asking the user for the first number via an input(variable named "start")
 end = input("What is your ending number?")
-#  Asking the user for the 2nd number via a input(variable named "end")
+#  Asking the user for the 2nd number via an input(variable named "end")
 increment = input("What is your third number(increment)?")
+# Asking the user for the 3rd number via an input
