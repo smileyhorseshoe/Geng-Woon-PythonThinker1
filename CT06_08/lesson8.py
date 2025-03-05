2f2ced9b47872b50c2a5253b8eac47ac0fe13d6b
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-05 08:00:33 +0000

    Mar 5, 2025, 8:00 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -8,4 +8,4 @@ start = input("What is your starting number?")
 end = input("What is your ending number?")
 #  Asking the user for the 2nd number via an input(variable named "end")
 increment = input("What is your third number(increment)?")
-# Asking the user for the 3rd number via an input
+# Asking the user for the 3rd number via an input(increment)
