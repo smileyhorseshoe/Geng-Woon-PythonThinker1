e6122d0c9d2fe3b1dc866d810d494b3dff8e78a0
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-05 07:53:05 +0000

    Mar 5, 2025, 7:53 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,4 +1,4 @@
 name = input("What is your name?")
-# Asking the user what their name is.
+# Asking the user what their name i via 'input'.
 print("Nice to meet you, " + name +"!")
 # Printing a welcome script with their name given in the input.
