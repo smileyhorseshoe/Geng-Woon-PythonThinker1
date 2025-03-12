42621a8dcf595bf6891eac7836583a21c2180e68
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 08:29:27 +0000

    Mar 12, 2025, 8:29 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -90,7 +90,7 @@
 # # TASK 4A:
 import random
 for i in range(2):
-    print()
+    print(random)
 # The output should be one of the following:
 # 1. True (If the answer is correct)
 # 2. False (If the answer is wrong)
