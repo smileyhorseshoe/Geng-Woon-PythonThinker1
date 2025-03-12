eae94f1d584eee4f079746ea1ad21361afd1d3ee
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 08:11:22 +0000

    Mar 12, 2025, 8:11 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -44,7 +44,7 @@
 # Import the 'random' library and create a program that randomly
 # output a number between 1 to 6
 import random
-num1 = random.ranint
+num1 = random.randint(1 , 6)
 # **Task 2b**:
 # Using the 'random' library, create 20 numbers between 0 and
 # 9999 randomly.
