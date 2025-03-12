68149fef780c777ebb7e6c9c2d211ad49e8f36d4
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 08:27:54 +0000

    Mar 12, 2025, 8:27 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -87,7 +87,7 @@
 # **Task 4a**: Math Question Generator
 # Using the 'random' library, generate 2 numbers between 1 and 50
 # that the user must add together.
-
+# 
 # The output should be one of the following:
 # 1. True (If the answer is correct)
 # 2. False (If the answer is wrong)
