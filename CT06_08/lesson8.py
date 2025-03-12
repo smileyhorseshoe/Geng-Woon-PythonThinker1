1f4648de77265f294330990db38db3b1e46214f2
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 07:36:17 +0000

    Mar 12, 2025, 7:36 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,20 +1,10 @@
-# name = input("What is your name?")
-# # Asking the user what their name i via 'input'.
-# print("Nice to meet you, " + name +"!")
-# # Printing a welcome script with their name given in the input.
-# 
-start = int(input("What is your starting number?"))
-# Asking the user for the first number via an input(variable named "start")
-end = int(input("What is your ending number?"))
-#  Asking the user for the 2nd number via an input(variable named "end")
-increment = int(input("What is your third number(increment)?"))
-# Asking the user for the 3rd number via an input(increment)
+# Lesson 8 - Importing Libraries, Boolean & Conditions
 
-#  Additional note: All numbers asked for has been converted to integers instead of strings.
+## Recap 1: Product of 5 numbers
 
-for i in range(start , end , increment):
-    print(i)
-    i = start + increment
-# Creating a repeat loop that adds the current number by the increment(e.g. 5 + 2 = 7 , 7 + 2 = 9) and stopping once the next multiple is the end number.
+# Write a program to calculate the product (multiplication) of 5
+# numbers.
 
-
+# 1. Using a for loop, ask the user for 5 numbers one at a time.
+# 2. Calculate the multiplication for these 5 numbers and print
+#    it out.
