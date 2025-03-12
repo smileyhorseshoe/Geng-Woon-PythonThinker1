697fb5d4834920da6761470cf2a48df4b387124e
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 08:35:22 +0000

    Mar 12, 2025, 8:35 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -92,8 +92,8 @@ import random
 num1 = random.randint(1 , 50)
 num2 = random.randint(1 , 50)
 total = num1 + num2
-# ans = input("What is " + str(num1) + "+" + str(num2) + "=")
-# print(ans = num1 + num2)
+ans = input("What is " + str(num1) + "+" + str(num2) + "=")
+print(ans == num1 + num2)
 # The output should be one of the following:
 # 1. True (If the answer is correct)
 # 2. False (If the answer is wrong)
