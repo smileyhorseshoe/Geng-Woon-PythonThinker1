011ba443e4b8b3d87a18cae48b282c8656e6de4d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-12 08:40:18 +0000

    Mar 12, 2025, 8:40 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -101,3 +101,17 @@ print(ans == total)
 # Example:
 # What is 2 + 5? << 7 >>
 # True
+
+
+# **Task 4b**: Range Guesser
+# Create a program that generates a random number between 1 and
+# 50.
+
+# The user should input a range (two numbers: start and end).
+
+# The program checks if the random number falls within the user's
+# range.
+
+# The output should be one of the following:
+# 1. True (If the answer is correct)
+# 2. False (If the answer is wrong)
