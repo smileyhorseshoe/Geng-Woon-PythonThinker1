507280c5d47616c89914a68b5d6b1a433056736d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 07:38:42 +0000

    Mar 19, 2025, 7:38 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -156,6 +156,5 @@ for i in range(numQuestions):
     num2 = random.randint(1 , 10)
     total = num1 + num2
     userAnswer = input("What is " + str(num1) + "+" + str(num2) + "?")
-    if userAnswer == total:
-        print("
+    print(userAnswer == total)
 # help
