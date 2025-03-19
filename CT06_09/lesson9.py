56bb12ed89b0ddb64bca3e0fd3bb619260d162cd
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 08:16:55 +0000

    Mar 19, 2025, 8:16 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -30,7 +30,4 @@ print("Number 3:" + str(num3))
 num1 % 2
 num2 % 2
 num3 % 2
-if num1 and num2 and num3 == 0 or num1 and num2 and num3 == 1:
-    print("True")
-else:
-    print("False")
+print(num1 and num2 and num3 == 0 or num1 and num2 and num3 == 0)
