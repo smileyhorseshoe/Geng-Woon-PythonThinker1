1a4035752536c4dfa908b4d74c5d27a244a47ec7
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 07:43:08 +0000

    Mar 19, 2025, 7:43 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -154,6 +154,7 @@ numQuestions = int(input("What is the number of questions you want to attempt?")
 for i in range(numQuestions):
     num1 = random.randint(1 , 10)
     num2 = random.randint(1 , 10)
+    total = num1 + num2
     print(total)
     userAnswer = input("What is " + str(num1) + "*" + str(num2) + "=")
     print(int(userAnswer) == total)
