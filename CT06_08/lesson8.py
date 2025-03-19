b40efdbbcac5f2d82ab9a3c659dd48d0c84ddf7a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 07:32:16 +0000

    Mar 19, 2025, 7:32 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -155,6 +155,6 @@ for i in range(numQuestions):
     num1 = random.randint(1 , 10)
     num2 = random.randint(1 , 10)
     total = num1 + num2
-    userAnswer = input()
+    userAnswer = input("What")
     print(userAnswer == total)
 # help:(
