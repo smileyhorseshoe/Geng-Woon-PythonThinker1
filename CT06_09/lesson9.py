6b3e4df1cad61c5e3d88c45c7c70058e2eca7c74
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 08:08:44 +0000

    Mar 19, 2025, 8:08 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -21,6 +21,4 @@
 # 6. Print if "All numbers are even/odd" is 'True' or 'False'
 import random
 
-for i in range(3):
-      i = random.randint(1 , 6)
-      print(i)
+num1 =   num1 = random.randint(1 , 10)
