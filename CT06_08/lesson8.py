0e1fc2182a52b07f72a0ee40f2dd0b38e8e20b0d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 07:39:39 +0000

    Mar 19, 2025, 7:39 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -155,6 +155,7 @@ for i in range(numQuestions):
     num1 = random.randint(1 , 10)
     num2 = random.randint(1 , 10)
     total = num1 * num2
+    print(total)
     userAnswer = input("What is " + str(num1) + "*" + str(num2) + "=")
     print(userAnswer == total)
 # help
