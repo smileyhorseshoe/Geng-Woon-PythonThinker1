57b62b912319781b1822eb96519045c615fddb74
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-19 08:04:35 +0000

    Mar 19, 2025, 8:04 AM

---
+++ CT06_09/lesson9.py
@@ -0,0 +1,21 @@
+# Recap 1: Dice Roll Simulator
+# Generate and print 3 random numbers between 1 and 6, followed
+# by an output of 'True' if all 3 numbers are either even or odd.
+
+# Example:
+# 1st number: 6
+# 2nd number: 4
+# 3rd number: 6
+# All numbers are even/odd: True
+
+# 1. Import the 'random' library
+# 2. Create 3 variables to hold a random number that is between
+#    1 and 6, generated using 'random.randint()'
+# 3. Using string concatenation, print the generated number for
+#    each of the 3 numbers
+# 4. Using the '%' and '==' operator, check if each number is
+#    divisible by 2 (remainder = 0)
+# 5. Using multiple '==' operators, a new variable 'all_even_odd'
+#    should be assigned 'True' if all 3 numbers are either all
+#    even or all odd numbers.
+# 6. Print if "All numbers are even/odd" is 'True' or 'False'
