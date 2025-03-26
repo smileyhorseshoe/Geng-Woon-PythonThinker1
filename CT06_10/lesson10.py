1bf29a3166ba914721fcf10bb9b5c1fa3f6b60db
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-26 08:21:27 +0000

    Mar 26, 2025, 8:21 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -7,8 +7,18 @@
 #         If it is, print "{number} is positive."
 # 3. Use an 'else' statement for when the number is not greater than 0.
 #         In this case, print "{number} is negative."
-number = int(input("Give me a number(NO DECIMALS!)"))
-if number > 0:
-    print("Your number is a positive number.")
-else:
-    print("Your number is a negative number.")
+# number = int(input("Give me a number(NO DECIMALS!)"))
+# if number > 0:
+#     print("Your number is a positive number.")
+# else:
+#     print("Your number is a negative number.")
+
+# Task 2: Random Number Guesser III
+# Create a Random Number Guesser program
+
+# 1. Generate a random integer between 1 to 10
+# 2. Ask the user to guess a number
+# 3. If the user guesses correctly:
+#     print "Congratulations!! You did it!"
+# 4. If the user guesses wrongly: 
+#     print "Oops, better luck next time!"
