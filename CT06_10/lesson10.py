bbb722dceba6fab26bbec58b893375b11c3d8388
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-26 08:49:07 +0000

    Mar 26, 2025, 8:49 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -80,8 +80,7 @@ userAge = int(input("What is your age?"))
 if userAge < 13:
     print("Child")
 else:
-    if userAge == 13 or 14 or 15 or 16 or 17 or 18:
+    if userAge < 19:
         print("Teen")
     else:
-        if userAge > 18:
-            print("Adult")
+        print("Adult")
