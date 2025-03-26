6d90d4e52819be7ef2e97b1da5ea6baaa7b7d0f0
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-26 08:29:41 +0000

    Mar 26, 2025, 8:29 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -40,7 +40,7 @@
 # 3. If the password matches, print "Login Successful"
 # 4. If the password does not match, print "Password Incorrect"
 
-pw = taco123!
+pw = "taco123!"
 
 userInput = input("What is the password?")
 if userInput == pw:
