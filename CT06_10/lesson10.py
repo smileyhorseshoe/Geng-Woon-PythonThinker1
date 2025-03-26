cd8f80b3bd4f13b5c24dc4badee51e745f33702e
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-03-26 08:17:46 +0000

    Mar 26, 2025, 8:17 AM

---
+++ CT06_10/lesson10.py
@@ -0,0 +1,9 @@
+# Task 1: Positive or Negative Numbers
+# Write a program that will let the user know if the number they
+# have entered is positive or negative.
+
+# 1. Ask the user to input a number
+# 2. Using an 'if' statement, check if the number is greater than 0
+#         If it is, print "{number} is positive."
+# 3. Use an 'else' statement for when the number is not greater than 0.
+#         In this case, print "{number} is negative."
