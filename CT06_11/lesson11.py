edabad1a25a486457994bf909d777a81cf2cf50f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-02 08:35:03 +0000

    Apr 2, 2025, 8:35 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -84,3 +84,12 @@
 #     print("Ticket Price:15 SGD.")
 # else:
 #     print("Ticket Price:20 SGD.")
+
+
+# Task 7: Colour filter (NOT)
+# Create a program that will ask the user for a colour and print
+# "Try again" if the input of the user is not "Green".
+
+# 1. Ask user for a colour
+# 2. Using the 'not' operator, check if input is not "Green".
+#    If true, print "Try again"
