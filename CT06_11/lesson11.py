08c48dd0591a1fdc221aeae1cc67e3b5c7448e28
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-02 07:38:22 +0000

    Apr 2, 2025, 7:38 AM

---
+++ CT06_11/lesson11.py
@@ -0,0 +1,12 @@
+# Lesson 11 - AND OR NOT
+
+# Recap 1: Purchase Advisor
+# Create a program that asks the user for the price of an item (px) and
+# gives a comment based on the price:
+
+# if:
+#     px <= 5: "Sounds good!"
+#     px <= 50: "Are you sure you need this?"
+#     px <= 500: "Where are you getting this money from?!"
+#     px > 500: "Don't even think about it!"
+
