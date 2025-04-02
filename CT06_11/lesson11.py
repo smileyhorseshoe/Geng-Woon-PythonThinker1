6e35db94b37c82b9b6928a72de47463920eb864f
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-02 08:06:05 +0000

    Apr 2, 2025, 8:06 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -48,3 +48,4 @@
 # 1. Ask the user to input a number
 # 2. If the number is both a multiple of 3 and a multiple of 7:
 #     print "The number is divisible by 3 and 7!"
+numberCheck = int(input("Give me a number in numerals."))
