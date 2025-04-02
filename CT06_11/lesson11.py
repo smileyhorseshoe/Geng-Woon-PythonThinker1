91fa60338fae9475d8d96577aa1c0bb66a79e0c4
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-02 08:00:00 +0000

    Apr 2, 2025, 8:00 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -9,13 +9,26 @@
 #     px <= 500: "Where are you getting this money from?!"
 #     px > 500: "Don't even think about it!"
 # RECAP
-px = float(input("What is the price of the item you want to buy?"))
+# px = float(input("What is the price of the item you want to buy?"))
 
-if px <= 5:
-    print("Sounds good!") 
-elif px <= 50:
-    print("Are you sure you need this?")
-elif px <= 500:
-    print("Where are you even getting this money from?!")
-else:
-    print("Don't even think about it!")
+# if px <= 5:
+#     print("Sounds good!") 
+# elif px <= 50:
+#     print("Are you sure you need this?")
+# elif px <= 500:
+#     print("Where are you even getting this money from?!")
+# else:
+#     print("Don't even think about it!")
+
+
+
+
+# Task 1: AND Operator in Simple Conditions (AND)
+# You are writing a program for an amusement park that needs to check
+# if both riders of a ride are above the height of 120cm. Use the 'and'
+# operator to determine if value of both 'rider1' and 'rider2' are
+# greater than 120.
+
+# rider1 = 125
+# rider2 = 150
+# >> True
