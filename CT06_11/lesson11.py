77381dcdd30826bf0cdcb4239094683407cd6d34
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-09 07:41:35 +0000

    Apr 9, 2025, 7:41 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -132,3 +132,4 @@ username_ans = "John123"
 password_ans = "pw123"
 username_ui = input("What is your username?")
 password_ui = input("What is your password?")
+if password_ui == password_ans and username_ans == username
