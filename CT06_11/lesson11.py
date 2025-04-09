ba5badd9c8d8e3dcdc4cbe9b50f9db8a8ad9bd4a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-09 07:46:19 +0000

    Apr 9, 2025, 7:46 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -155,4 +155,5 @@
 # 3. If the condition is 'True', print "Game in progress...".
 # 4. Otherwise, print "Game is paused or inactive."
 game_status = "active"
-if gamestatus == active and not game_status == "paused"
+if gamestatus == "active" and not game_status == "paused" or "inactive":
+    print("Game in progress..")
