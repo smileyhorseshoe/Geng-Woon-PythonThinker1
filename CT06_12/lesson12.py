1d58fb54fb78a7ee58126db20394f6e0569840e4
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-09 08:49:09 +0000

    Apr 9, 2025, 8:49 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -131,3 +131,7 @@ num2 = random.randint(1 , 10)
 ans = num1 + num2
 while True:
     question = input("What is:" + str(num1) + "+" + str(num2) + "?")
+    if question == ans:
+        print("Correct!")
+    else:
+        pass
