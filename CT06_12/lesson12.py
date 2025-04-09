83ef99ee45fba96631a7f630b1e2cd93f14d702c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-09 08:08:09 +0000

    Apr 9, 2025, 8:08 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -31,9 +31,15 @@
 #     visitor += 1
 # print("The venue has reached its max capacity! Do not accept anymore visitors!")
 # 1b.
+# maxVisitor = 30
+# visitorPresence = 18
+# while visitorPresence < maxVisitor:
+#     visitorPresence =+ 1
+#     print("Visitor Count:" + str(visitorPresence))
+# print("The venue has reached its max capacity! Do not accept anymore visitors!")
 maxVisitor = 30
-visitorPresence = 18
-while visitorPresence < maxVisitor:
-    visitorPresence =+ 1
-    print("Visitor Count:" + str(visitorPresence))
-print("The venue has reached its max capacity! Do not accept anymore visitors!")
+# visitorPresence = 18
+# while visitorPresence < maxVisitor:
+#     visitorPresence =+ 1
+#     print("Visitor Count:" + str(visitorPresence))
+# print("The venue has reached its max capacity! Do not accept anymore visitors!")
