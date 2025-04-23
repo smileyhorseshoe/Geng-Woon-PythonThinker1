a390537d45b0b5cd469f78f1326b604a5da5221d
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-23 08:41:26 +0000

    Apr 23, 2025, 8:41 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -3,8 +3,8 @@ import turtle
 window = turtle.Screen()
 window.setup(width=600, height=400)
 t = turtle.Turtle()
-turtle.shapes(turtle)
-turtle.fillcolor("orange")
+t.shapes(turtle)
+t.fillcolor("orange")
 
 
 
