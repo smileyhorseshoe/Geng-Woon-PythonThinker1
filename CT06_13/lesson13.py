1346b7b2883168a8c1c7451ecf7bc4a9956b9aaf
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-23 07:56:25 +0000

    Apr 23, 2025, 7:56 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -134,6 +134,6 @@ for i in range(10):
     winners.append(i)
 num = 1
 while num < 10:
-    print("Winner #" + str(num) + print(winnersstr([num])))
+    print("Winner #" + str(num) + print(winners[num]))
     num =+ 1
 
