9165429002c9ab88207b13f7c5b3e811e95d9dec
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-23 09:00:38 +0000

    Apr 23, 2025, 9:00 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -7,9 +7,11 @@ t.shape("turtle")
 t.fillcolor("orange")
 t.seth(0)
 t.down()
+# SQUARE
 # for i in range(4):
 #     t.left(90)
 #     t.forward(50)
+# HEXAGON
 for i in range(6):
     t.left(60)
     t.forward(50)
