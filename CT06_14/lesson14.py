9533106df04d0a3a2dd8829c269b3676ef5b00b4
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-23 08:28:32 +0000

    Apr 23, 2025, 8:28 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,5 +1,5 @@
-# print("Hello from lesson 14")
-# import turtle
-# window = turtle.Screen()
-# window.setup(width=600, height=400)
-# window.mainloop()
+print("Hello from lesson 14")
+import turtle
+window = turtle.Screen()
+window.setup(width=600, height=400)
+window.mainloop()
