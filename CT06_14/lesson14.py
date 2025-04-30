c46d7e6db8f29bd0b8d59e8359dfb5d6a1e00a9a
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-30 08:18:20 +0000

    Apr 30, 2025, 8:18 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -35,7 +35,10 @@ for i in range(10):
     setx = random.randint(-280, 280)
     sety = random.randint(-280, 280)
     t.goto(setx , sety)
-    for 
+    # Square
+# for i in range(3):
+#     t.forward(50)
+#     t.left(120)
 
 
 
