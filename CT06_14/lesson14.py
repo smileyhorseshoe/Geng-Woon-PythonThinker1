8a7864b73753f1fc7da4c268867836d728b02194
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-30 07:43:55 +0000

    Apr 30, 2025, 7:43 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -5,8 +5,8 @@ window.setup(width=600, height=400)
 t = turtle.Turtle()
 t.fillcolor("orange")
 t.shape("turtle")
-for i in range(6):
-    t.left(60)
+for i in range(5):
+    t.left(72)
     t.forward(50)
 
 
