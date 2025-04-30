df0456e8d48b8dabc207ef5b8a9b12a5ac3a4871
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-04-30 07:38:12 +0000

    Apr 30, 2025, 7:38 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,20 +1,4 @@
 # print("Hello from lesson 14")
-import turtle
-window = turtle.Screen()
-window.setup(width=600, height=400)
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
-t.seth(0)
-t.down()
-# SQUARE
-# for i in range(4):
-#     t.left(90)
-#     t.forward(50)
-# HEXAGON
-# for i in range(6):
-#     t.left(60)
-#     t.forward(50)
 
 
 
@@ -37,4 +21,50 @@ t.down()
 
 
 
-window.mainloop()
+
+
+
+
+
+
+
+
+# import turtle
+# window = turtle.Screen()
+# window.setup(width=600, height=400)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
+# t.seth(0)
+# t.down()
+# # SQUARE
+# # for i in range(4):
+# #     t.left(90)
+# #     t.forward(50)
+# # HEXAGON
+# # for i in range(6):
+# #     t.left(60)
+# #     t.forward(50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# window.mainloop()
