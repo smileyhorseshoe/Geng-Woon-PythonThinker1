ba636f9ab2970389c124a5cfc31d7870d1308254
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-07 07:54:28 +0000

    May 7, 2025, 7:54 AM

--- CT06_14/sem1q2.py
+++ CT06_14/sem1q2.py
@@ -1,1 +1,17 @@
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+# 1. Write code below to print the 3rd item 
+#    in this list using index e.g. earth
+
+
+# 2. Write code to append neptune to this list.
+
+
+# 3. Elon Musk has conquered Mars. 
+#    Rename Mars in the list to be "muskworld"
+
+
+# 4. Remove uranus from this list.
+
+
+# 5. Using a for loop, print all the planets 
+#    from this list one by one.
