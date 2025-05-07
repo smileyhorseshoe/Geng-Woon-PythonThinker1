d0a8d0eaa91abf5458ad83d8961a4d3630933bef
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-07 08:21:21 +0000

    May 7, 2025, 8:21 AM

--- CT06_14/sem1q2.py
+++ CT06_14/sem1q2.py
@@ -1,7 +1,7 @@
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
-#  planets = mercury, venus, earth, muskworld, jupiter,
+#  planets = mercury, venus, earth, muskworld, jupiter, saturn
 
 # 2. Write code to append neptune to this list.
 
