ff8e7e6591d9d59079a84510745e53c747f61fbb
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-07 08:09:35 +0000

    May 7, 2025, 8:09 AM

--- CT06_14/sem1q2.py
+++ CT06_14/sem1q2.py
@@ -15,6 +15,7 @@ planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 
 # 5. Using a for loop, print all the planets 
 #    from this list one by one.
+
 print(planets[2])
 planets.append("neptune")
 planets.remove("mars")
