72bf30aad49688b350cde82d75419d055eb787fe
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-07 07:47:01 +0000

    May 7, 2025, 7:47 AM

---
+++ CT06_14/sem1q1.py
@@ -0,0 +1,8 @@
+#Task 1
+# Print numbers from 10 to 200 using the while loop
+# Your numbers must be in multiples of 10.
+# 10 must be first number printed, and 200 the last number.
+
+# Example: 10, 20, 30 ..... 180, 190, 200.
+# Note that the numbers do not need to be printed in one line.
+# Write your code here
