cd432931438e42b1141760f456a065ded2438a50
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-14 08:09:58 +0000

    May 14, 2025, 8:09 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -29,9 +29,4 @@
 # if name == "Ethan":
 #     Ethan()
 # elif name == "Ben":
-                                                                                                                                                                          
-                   
-                                                                                                                              
-                                                                                                                                                      
-                                                                                                                                                                                        
-                                                                                                                                  
+                                                           
