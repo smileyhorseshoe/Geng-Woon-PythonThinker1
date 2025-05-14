47bbb74fe211ef812ed6c051372fad65fccb545c
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-14 07:46:34 +0000

    May 14, 2025, 7:46 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,1 +1,17 @@
-# print("Hello from lesson 15") 
+## Task 1: Self-introduction
+# You are at a party, and you expect to see your friend Ethan and 3 of
+# his friends you have never met before. They are Ben, Gracie, and
+# Javior.
+
+# Write a program (with or without functions) that will ask the user
+# for their name and print 1 of 3 ways to greet the person:
+# 1. If the person is Ethan, greet him by saying:
+#         "Hi Ethan. How are you?"
+# 2. If the person is either Ben, Gracie, or Javior, greet them by
+#    saying:
+#         "Hi there!"
+#         "My name is Freddo"
+#         "I like to swim and eat chicken wings!"
+#         "Nice to meet you!"
+# 3. If the person is none of the above, say:
+#         "I don't think you belong here..."
