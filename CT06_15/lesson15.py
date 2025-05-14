f24152502ef9fe9e875ce63f1cb0f70db89e5eb3
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-14 07:55:41 +0000

    May 14, 2025, 7:55 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -15,7 +15,7 @@
 #         "Nice to meet you!"
 # 3. If the person is none of the above, say:
 #         "I don't think you belong here..."
-functionu# def greetings():
+# def greetings():
 #     name = input("What is your name?")
 # def Ethan():
 #     print("Hi Eathen. How are you?")
