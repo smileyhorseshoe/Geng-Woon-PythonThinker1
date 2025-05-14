ea96fe46fa5a46be3e503c9eecbdb3a9ca3b16b0
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-05-14 08:10:25 +0000

    May 14, 2025, 8:10 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -29,3 +29,16 @@
 # if name == "Ethan":
 #     Ethan()
 # elif name == "Ben":
+
+## Task 3: Increment Counter
+# Create a function that will increase the 'counter' variable by 1 each
+# time it is called.
+
+# 1. Create a 'counter' variable and assign it '0'
+# 2. Define a function 'increment_counter()':
+#         a. Declare 'counter' as 'global'
+#         b. Add 1 to 'counter'
+# 3. Test your program out by calling the 'increment_counter()' function
+# 4. 3 times before printing out the value of the 'counter' variable.
+
+# Your output should be "3"
